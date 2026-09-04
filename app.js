c552f3de1312b937f33f8f00490fc2c258668b42
orihiro0518 <masterxx007@gmail.com>
2026-09-05 08:30:58 +0900

    Load quiz navigation UX fixes

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-document.write('<script src="app-core.js"><\/script><script src="app-explanations.js"><\/script><script src="app-quiz.js"><\/script>');
+document.write('<script src="app-core.js"><\/script><script src="app-explanations.js"><\/script><script src="app-quiz.js"><\/script><script src="app-ui.js?v=341"><\/script>');
